b902c1dd8ed9cbc57ffa3db1e9f4e9c67a7dd3d3
DariaDolia <daria.dolia1@gmail.com>
2022-03-13 22:36:47 +0100

    added more solutions(2)

--- massiv.py
+++ massiv.py
@@ -13,3 +13,38 @@ def masiv(mas, k):
 
 
 masiv([-1, 2, 3, 4], 22)
+
+
+def sum_of_two(mas, k):
+    for i in range(len(mas)):
+        summa = mas[0] + mas[-1]
+        if summa > k:
+            del mas[-1]
+        elif summa < k:
+            del mas[0]
+        elif summa == k:
+            print(f'Answer: {mas[0]} and {mas[-1]}')
+            break
+        else:
+            print([])
+
+
+sum_of_two([-1, 0, 1, 3, 5, 7], 1)
+
+
+def sum_two_num(mas, k):
+    start = 0
+    end = len(mas) - 1
+    while start != end:
+        summa = mas[start] + mas[end]
+        if summa == k:
+            print(f'Answer: {mas[start]} and {mas[end]}')
+            return
+        elif summa > k:
+            end -= 1
+        else:
+            start += 1
+    print([])
+
+
+sum_two_num([-1, 4, 7, 7, 7], 3)
